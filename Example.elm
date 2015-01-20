afe94c25fd0079721fa9dd0772991b250919c6a3
Izaak Meckler <ihmeckler@gmail.com>
2015-01-20 17:27:15 -0600

    updated example

--- Example.elm
+++ Example.elm
@@ -9,9 +9,9 @@ import Signal
 import Color
 import Debug
 
-pos =  Stage.for (3 * second) (\t -> t * (40 / second))
+pos =  Stage.cycle
+    <| Stage.for (3 * second) (\t -> t * (40 / second))
     +> \x -> Stage.for (1 * second) (\t -> x - t * (x / (1 * second)))
-    <> Stage.forever (\_ -> 0)
 
 main = Stage.run (Signal.constant pos) (Time.every 30)
   |> Signal.map (\x -> filled Color.red (circle 20) |> moveX x)
